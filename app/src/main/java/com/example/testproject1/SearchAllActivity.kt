2be163be1fbe6a5f0542468cc7c7b7dfa948f2dd
SongGol <dldmsgus2@gmail.com>
2021-07-04 09:19:07 +0900

    상단 타이틀바 제거

--- app/src/main/java/com/example/testproject1/SearchAllActivity.kt
+++ app/src/main/java/com/example/testproject1/SearchAllActivity.kt
@@ -1,4 +1,11 @@
 package com.example.testproject1
 
-class SearchAllActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchAllActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_all)
+    }
 }
